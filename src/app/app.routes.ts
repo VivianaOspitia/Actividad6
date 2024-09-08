7114c73c75400e8d4470ffedcb2a66b1cff994d9
VivianaOspitia <vivi1857@hotmail.com>
2024-09-08 01:17:22 -0500

    se agrega el enrutamiento de newuser y de user/ID

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,26 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component'; 
+import { FormComponent } from './components/form/form.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
 
 export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/newuser', 
+        pathMatch: 'full' 
+    },
     {
         path: 'home',
         component: HomeComponent,
         title: 'Home page',
+    },
+    {
+        path: 'newuser',
+        component: FormComponent,
+        title: 'Crear un nuevo usuario'
+    },
+    {
+        path: 'user/:id',
+        component: UserDetailsComponent
     }
 ];
